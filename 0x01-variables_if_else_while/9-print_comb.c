481bc43d132f6fca5c92d331d57e69d40f46eff2
SandraWarugongo <Sandra.wwarugongo@gmail.com>
2023-10-09 05:42:04 -0700

    Preprocessor

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 /*
  * File: 9-print_comb.c
- * Auth: Sandra
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
@@ -17,14 +17,22 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
+		if (num != 9)
+		{	
 		
 		putchar(',');
 		putchar(' ');
+         
+	 	}	
+	
 	}
-
 	putchar('\n');
 
 	return (0);
 }
+
+
+
+
+
+
